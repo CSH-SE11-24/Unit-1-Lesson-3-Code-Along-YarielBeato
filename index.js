b1faa1ad69db7ca52bdcb509022fa2e34ca3bfc2
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 17:22:27 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Code Along Norms: 
+// 1. Only be on Repl.it/classroom resources
+// 2. Actively follow along and engage
+// 3. Ask questions and flag if you get stuck
+// 4. Take notes as needed!
+
+// USER INPUT
+// Prompt for name
+
+// STRING MANIPULATION 
+// Concatenation: with a + sign you need to add a space
+
+// With a , sign, spaces added automatically
+
+// String interpolation: with backticks, you can input variables directly
+
+
+
+// CASTING
+// Prompt assumes the input is a string
+
+// To change it to a number, use parseInt/parseFloat
+
+
+// Check the type of the variable
+
